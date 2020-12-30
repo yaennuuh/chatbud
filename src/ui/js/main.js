a38b06de6b13f1767d344f94f5c25bb407920cf7
unknown <management@djbarrex.com>
2020-12-30 13:01:39 +0100

    Make it dynamic loading ui

--- src/ui/js/main.js
+++ src/ui/js/main.js
@@ -1,56 +1,70 @@
 var ipc = require('electron').ipcRenderer;
+var _ = require('lodash');
 
 window.onload = function () {
-    console.log('loaded');
     var closeButton = document.getElementById('close-button');
     closeButton.addEventListener('click', function () {
-        console.log('i got clicked');
         ipc.send('close-application', '');
     });
-    createWebComponent('ui');
-    var closeButton = document.getElementById('add-button');
-    closeButton.addEventListener('click', function () {
-        loadCustomTag('ui');
+    var pluginList = [{
+        "name": "TwitchTestPlugin",
+        "folder": "TwitchTestPlugin",
+        "ui-html": "ui.html",
+        "ui-js":  "ui.js"
+    }];
+    pluginList.forEach(plugin => {
+        plugin.tagname = _.kebabCase(plugin.name);
+        createWebComponent(plugin);
+    });
+    //createWebComponent('ui');
+    var addButton = document.getElementById('add-button');
+    addButton.addEventListener('click', function () {
+        var componentnamefield = document.getElementById('componentname');
+        console.log(componentnamefield.value);
+        loadCustomTag(componentnamefield.value);
     });
 }
 
 function loadCustomTag(pluginName) {
-    document.getElementById('content').appendChild(
+    pluginName = _.kebabCase(pluginName);
+    content = document.getElementById('content');
+    content.innerHTML = '';
+    content.appendChild(
         document.createElement(`custom-${pluginName}`));
 }
 
-function createWebComponent(pluginName) {
-    fetch(`../plugins/TwitchTestPlugin/${pluginName}.html`)
+function createWebComponent(plugin) {
+    fetch(`../plugins/${plugin['folder']}/${plugin['ui-html']}`)
         .then(stream => stream.text())
         .then(text => {
-            createTemplateTag(text, pluginName);
+            createTemplateTag(text, plugin);
         });
 }
 
-function createTemplateTag(html, pluginName) {
+function createTemplateTag(html, plugin) {
     var templateTag = document.createElement('template');
-    templateTag.id = `custom-${pluginName}-template`;
+    templateTag.id = `custom-${plugin['tagname']}-template`;
     templateTag.innerHTML = html;
     document.getElementById("template-holder").appendChild(templateTag);
-    loadTemplate(pluginName);
+    loadTemplate(plugin);
 }
 
-function loadTemplate(pluginName) {
-    customElements.define(`custom-${pluginName}`,
+function loadTemplate(plugin) {
+    customElements.define(`custom-${plugin['tagname']}`,
         class extends HTMLElement {
 
             constructor() {
                 super();
                 const template = document
-                    .getElementById(`custom-${pluginName}-template`)
+                    .getElementById(`custom-${plugin['tagname']}-template`)
                     .content;
                 this._shadowRoot = this.attachShadow({ mode: 'open' });
                 this._shadowRoot.appendChild(template.cloneNode(true));
-                this._pluginName = pluginName;
+                this._plugin = plugin;
             }
 
             connectedCallback() {
-                var CustomFunction = require(`../plugins/TwitchTestPlugin/${this._pluginName}.js`);
+                var CustomFunction = require(`../plugins/${this._plugin['folder']}/${this._plugin['ui-js']}`);
                 new CustomFunction(this._shadowRoot);
             }
         }
