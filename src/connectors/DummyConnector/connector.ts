8da7f88342d28c1863eb16e6cfc92473043ff531
unknown <management@djbarrex.com>
2020-12-31 13:03:29 +0100

    fix the dummy connector

--- src/connectors/DummyConnector/connector.ts
+++ src/connectors/DummyConnector/connector.ts
@@ -1,20 +1,19 @@
 import { IConnector } from "../../core/connectors/IConnector";
 import { CoreBot } from "../../core/CoreBot";
-import { IEvent } from "../../core/events/IEvent";
 import { Event } from "../../core/events/Event";
-import {EventData} from "../../core/events/EventData";
+import { EventData } from "../../core/events/EventData";
+import { IEvent } from "../../core/events/IEvent";
 
 class DummyConnector implements IConnector {
 
     start(): void {
-        let eventData = new EventData("[#loop 2] something[#loop 2] nix[/#loop][/#loop][#if $username == $username]dfsdfsfds[#else]asd[/#if] matching wait [#wait 5] not matching [/#wait] [#wait 2] matching wait [/#wait] sdfdsf[#if user == zwei] zweites if [#else] zweites else [/#if]");
         setTimeout(function () {
-            CoreBot.getInstance().notifyPluginsOnEventBusIn(new Event('twitch-chat-message', eventData));
+            CoreBot.getInstance().notifyPluginsOnEventBusIn(new Event('dummy-chat-message', new EventData('test')));
         }, 5000);
     }
 
     register(): string[] {
-        return ['twitch-chat-message'];
+        return ['dummy-chat-message'];
     }
 
     execute(event: IEvent): void {
--- src/plugins/DummyPlugin/plugin.ts
+++ src/plugins/DummyPlugin/plugin.ts
@@ -1,3 +1,5 @@
+import { Event } from "../../core/events/Event";
+import { EventData } from "../../core/events/EventData";
 import { IEvent } from "../../core/events/IEvent";
 import { IPlugin } from "../../core/plugins/IPlugin";
 import { PluginHelper } from "../../core/plugins/PluginHelper";
@@ -9,11 +11,12 @@ class DummyPlugin implements IPlugin {
 
     register(pluginHelper: PluginHelper) {
         this.pluginHelper = pluginHelper;
-        return ['twitch-chat-message'];
+        return ['dummy-chat-message'];
     }
 
     execute(event: IEvent) {
-        this.pluginHelper.sendEventToBusOut(event);
+        let eventData = new EventData("[#loop 2]something [#loop 2]nix [/#loop][/#loop][#if $username == $username]dfsdfsfds[#else]asd[/#if] matching wait [#wait 5] not matching [/#wait] [#wait 2] matching wait [/#wait] sdfdsf[#if user == zwei] zweites if [#else] zweites else [/#if]");
+        this.pluginHelper.sendEventToBusOut(new Event('dummy-chat-message', eventData));
     }
 
     getDummy(): string {
