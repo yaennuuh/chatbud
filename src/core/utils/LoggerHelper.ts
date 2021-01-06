4d6ed6afe9ec02407bf0fa65d24d2d05000d2f60
saemik94 <76621625+saemik94@users.noreply.github.com>
2021-01-06 21:58:22 +0100

    Logger plugin (#5)
    * add loger plugin
    
    * get logger at start
    
    * show all log files
    
    * check if path exists
    
    * clean up
    
    * replace logger
    
    * fix copy paste issue
    
    * move get logger helper
    
    * clean up interface
    
    * beautify

---
+++ src/core/utils/LoggerHelper.ts
@@ -0,0 +1,36 @@
+import {Logger} from "@tsed/logger";
+
+export class LoggerHelper {
+
+    static getLogger = (name?: string): Logger => {
+        const logger = new Logger(name);
+        const logFilePath = `${__dirname}/../../logs/`;
+        logger.appenders
+            .set("console", {
+                type: "stdout",
+                levels: ["DEBUG", "INFO", "WARN", "ERROR"]
+            })
+            .set("debug", {
+                type: "file",
+                filename: `${logFilePath}/debug.log`,
+                maxLogSize: 10*1024*1024,
+                layout: {type: "basic"},
+                levels: ["DEBUG", "INFO", "WARN", "ERROR"]
+            })
+            .set("error", {
+                type: "file",
+                filename: `${logFilePath}/error.log`,
+                maxLogSize: 10*1024*1024,
+                layout: {type: "basic"},
+                levels: ["ERROR"]
+            })
+            .set("info", {
+                type: "file",
+                filename: `${logFilePath}/app.log`,
+                maxLogSize: 10*1024*1024,
+                layout: {type: "basic"},
+                levels: ["INFO"]
+            });
+        return logger;
+    }
+}
