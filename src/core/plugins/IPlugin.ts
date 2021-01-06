227eba303449ecb07ea9e36ea9021665f41f01d5
Samuel Kohler <admin@samuelkohler.ch>
2021-01-06 21:49:28 +0100

    clean up interface

--- src/core/plugins/IPlugin.ts
+++ src/core/plugins/IPlugin.ts
@@ -1,8 +1,7 @@
 import { INotifiable } from "../INotifiable";
 import { PluginHelper } from "./PluginHelper";
-import {Logger} from "@tsed/logger";
 
 export interface IPlugin extends INotifiable {
     pluginHelper: PluginHelper;
-    register(pluginHelper: PluginHelper, logger?: Logger): string[];
+    register(pluginHelper: PluginHelper): string[];
 }
