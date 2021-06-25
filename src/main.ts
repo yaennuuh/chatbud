0a2e2998286cf711fdb8e7d0549779bc93358920
Yannick Annaheim <me@yannickannaheim.ch>
2021-06-26 00:36:00 +0200

    added updater

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
 
 export default class Main {
     static mainWindow: Electron.BrowserWindow;
@@ -31,6 +32,16 @@ export default class Main {
         Main.mainWindow.loadURL('file://' + __dirname + '/ui/loading.html');
         setTimeout(() => Main.mainWindow.loadURL('file://' + __dirname + '/ui/main.html'), 5000)
         Main.mainWindow.on('closed', Main.onClose);
+        Main.mainWindow.once('ready-to-show', () => {
+            autoUpdater.checkForUpdatesAndNotify();
+        });
+
+        autoUpdater.on('update-available', () => {
+            Main.mainWindow.webContents.send('update_available');
+        });
+        autoUpdater.on('update-downloaded', () => {
+            Main.mainWindow.webContents.send('update_downloaded');
+        });
 
         //Main.mainWindow.maximize();
         //Main.mainWindow.webContents.openDevTools();
@@ -41,6 +52,12 @@ export default class Main {
     static init() {
         ipcMain.on('close-application', function (event, data) {
             Main.mainWindow.close();
+        });
+        ipcMain.on('app_version', (event) => {
+            event.sender.send('app_version', { version: app.getVersion() });
+        });
+        ipcMain.on('restart_app', () => {
+            autoUpdater.quitAndInstall();
         });
     };
 
